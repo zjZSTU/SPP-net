d1721a5404b5ad4fcbe41f0ef25146a842c96842
zhujian <505169307@qq.com>
2020-03-28 21:44:04 +0800

    feat(alexnet_spp): 新建空间金字塔池化层，修改AlexNet模型

---
+++ py/models/alexnet_spp.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/3/28 下午4:27
+@file: alexnet_spp.py
+@author: zj
+@description: 
+"""
+
+import torch
+import torch.nn as nn
+from torchvision.models import alexnet
+
+from models.spatial_pyramid_pooling import SpatialPyramidPooling
+
+
+class AlexNet_SPP(nn.Module):
+
+    def __init__(self, num_classes=1000):
+        super(AlexNet_SPP, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            # nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        # 默认使用3级金字塔，其向量长度为21
+        self.spp = SpatialPyramidPooling()
+        # self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 21, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        # x = self.avgpool(x)
+        x = self.spp(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
+def alexnet_spp(num_classes=20):
+    model = AlexNet_SPP(num_classes=num_classes)
+    model_alexnet = alexnet(pretrained=True, progress=True)
+
+    pretrained_dict = model_alexnet.state_dict()
+    model_dict = model.state_dict()
+
+    res_dict = dict()
+    for item in zip(pretrained_dict.items(), model_dict.items()):
+        pretrained_dict_item, model_dict_item = item
+
+        k1, v1 = pretrained_dict_item
+        k2, v2 = model_dict_item
+        # print(k1, k2)
+        # print(v1.shape, v2.shape)
+
+        if k1 == k2 and v1.shape == v2.shape:
+            res_dict[k2] = v1
+        else:
+            res_dict[k2] = v2
+
+    model_dict.update(res_dict)
+    model.load_state_dict(model_dict)
+
+    return model
+
+
+def test():
+    model = AlexNet_SPP(num_classes=20)
+    print(model)
+
+    input = torch.randn((128, 3, 227, 227))
+    output = model.forward(input)
+    print(output.size())
+
+
+if __name__ == '__main__':
+    model = alexnet_spp(num_classes=20)
+    print(model)
---
+++ py/models/spatial_pyramid_pooling.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/3/28 下午4:24
+@file: spatial_pyramid_pooling.py
+@author: zj
+@description: 
+"""
+
+from math import sqrt
+import torch
+import torch.nn as nn
+
+
+class SpatialPyramidPooling(nn.Module):
+    """Generate fixed length representation regardless of image dimensions
+    Based on the paper "Spatial Pyramid Pooling in Deep Convolutional Networks
+    for Visual Recognition" (https://arxiv.org/pdf/1406.4729.pdf)
+    :param [int] num_pools: Number of pools to split each input feature map into.
+        Each element must be a perfect square in order to equally divide the
+        pools across the feature map. Default corresponds to the original
+        paper's implementation
+    :param str mode: Specifies the type of pooling, either max or avg
+    """
+
+    def __init__(self, num_pools=(1, 4, 16), mode='max'):
+        super(SpatialPyramidPooling, self).__init__()
+        self.name = 'SpatialPyramidPooling'
+        if mode == 'max':
+            pool_func = nn.AdaptiveMaxPool2d
+        elif mode == 'avg':
+            pool_func = nn.AdaptiveAvgPool2d
+        else:
+            raise NotImplementedError(f"Unknown pooling mode '{mode}', expected 'max' or 'avg'")
+        self.pools = []
+        for p in num_pools:
+            side_length = sqrt(p)
+            if not side_length.is_integer():
+                raise ValueError(f'Bin size {p} is not a perfect square')
+            self.pools.append(pool_func(int(side_length)))
+
+    def forward(self, feature_maps):
+        """Pool feature maps at different bin levels and concatenate
+        :param torch.tensor feature_maps: Arbitrarily shaped spatial and
+            channel dimensions extracted from any generic convolutional
+            architecture. Shape ``(N, C, H, W)``
+        :return torch.tensor pooled: Concatenation of all pools with shape
+            ``(N, C, sum(num_pools))``
+        """
+        assert feature_maps.dim() == 4, 'Expected 4D input of (N, C, H, W)'
+        batch_size = feature_maps.size(0)
+        channels = feature_maps.size(1)
+        pooled = []
+        for p in self.pools:
+            pooled.append(p(feature_maps).view(batch_size, channels, -1))
+        return torch.cat(pooled, dim=2)
